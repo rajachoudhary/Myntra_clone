1a75f930044b96b6a5b25814218402544eefab2e
rajachoudhary <rajachoudhary.com>
2022-05-04 19:18:44 +0530

    filter part of product page

--- myntra_clone/src/Components/Pages/Product.jsx
+++ myntra_clone/src/Components/Pages/Product.jsx
@@ -1,48 +1,13 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import {  sort } from '../../Redux/action'
+import React from 'react'
+import { Navbar } from '../Landing Page/Navbar'
+import { ProductTopNavbar } from '../ProductPage/ProductTopNavbar'
 
 export const Product = () => {
-  const dispatch = useDispatch()
-  const item = useSelector(state =>state)
-  console.log("Raja" ,item.item)
-  // const [value,setValue] = useState("")
-  // const filterData = (e)=>{
-  //   if(e.target.value === "highToLow"){
-  //     console.log(e.target.value)
-  //     dispatch(highToLowData(data))
-  //   } else {
-  //     console.log("raja")
-  //   }
-  // }
+ 
   return (
     <div>
-      <div>
-        <div>FILTERS</div>
-        <div>
-          <div>better Cotton Initiative</div>
-          <div>Bundles</div>
-          <div>Country of origin</div>
-          <div>Distress</div>
-          <div>Fabric</div>
-          <div>Fabric2</div>
-          <div>Fade</div>
-        </div>
-        <div>
-        <select onChange={(e)=>{
-          dispatch(sort(e.target.value))
-          
-        }}>
-          <option value="">Sort by : Recommended</option>
-          <option value="">What's New</option>
-          <option value="">Popularity</option>
-          <option value=" ">Better Discount</option>
-          <option value="discountRate" >Price : High to Low</option>
-          <option value="">Price : Low to High</option>
-          <option value="">Customer Rating</option>
-        </select>
-        </div>
-      </div>
+      <Navbar/>
+      <ProductTopNavbar/>
     </div>
   )
 }
---
+++ myntra_clone/src/Components/ProductPage/ProductTopNavbar.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {  sort } from '../../Redux/action'
+import style from "../../css/productNavbar.module.css"
+
+export const ProductTopNavbar = () => {
+  const dispatch = useDispatch()
+  const item = useSelector(state =>state)
+  
+  return (
+    <div>
+      <div className={style.topFilterNavbar}>
+        <div className={style.filterText}>FILTERS</div>
+        <div className={style.filterNavbar}>
+          <div >
+            <select name="" id="" >
+              <option value="" >better Cotton Initiative</option>
+            </select>
+          </div>
+          <div>
+            <select name="" id="">
+              <option value="">Bundles</option>
+            </select>
+          </div>
+          <div>
+            <select name="" id="">
+              <option value="">Country of origin</option>
+            </select>
+          </div>
+          <div>
+          <select name="" id="">
+              <option value="">Distress</option>
+            </select>
+          </div>
+          <div>
+            <select name="" id="">
+              <option value="">Fabric</option>
+            </select>
+          </div>
+          <div>
+            <select name="" id="">
+              <option value="">Fabric2</option>
+            </select>
+          </div>
+          <div>
+            <select name="" id="">
+              <option value="">Fade</option>
+            </select>
+          </div>
+          <div>
+            <select className={style.filterSection} onChange={(e)=>{dispatch(sort(e.target.value)) }}>
+              <option value="">Sort by : <span>Recommended</span></option>
+              <option value="">What's New</option>
+              <option value="">Popularity</option>
+              <option value=" ">Better Discount</option>
+              <option value="discountRate" >Price : High to Low</option>
+              <option value="">Price : Low to High</option>
+              <option value="">Customer Rating</option>
+            </select>
+          </div>
+          
+        </div>
+      </div>
+      <hr />
+    </div>
+  )
+}
+
